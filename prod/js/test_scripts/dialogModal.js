340df5a2429f79b59425404f93b97629c5f54a08
staticBanter <staticbanter@gmail.com>
2021-03-16 20:41:57 -0400

    Compiled output from src

---
+++ prod/js/test_scripts/dialogModal.js
@@ -0,0 +1,45 @@
+"use strict";
+(() => {
+    function toggleModal(target) {
+        const modals = document.body.querySelectorAll(target);
+        if (!modals) {
+            throw new Error("-toggleModal()- Could not find target modal");
+        }
+        modals.forEach((modal) => {
+            console.log(modal.getAttribute("open"));
+            if (modal.getAttribute("open") === "open" || modal.dataset.open === "open") {
+                modal.removeAttribute("open");
+                modal.dataset.toggleOpen = "closed";
+            }
+            else {
+                modal.setAttribute("open", "open");
+                modal.dataset.toggleOpen = "open";
+            }
+        });
+        return modals;
+    }
+    function toggleModalEventListener(event) {
+        if (!event.target) {
+            throw new Error("-ToggleModalEventListener()- A toggle modal event was called but the event had no source");
+        }
+        if (event.target instanceof HTMLElement) {
+            const toggle = event.target;
+            if (!toggle.dataset.target) {
+                throw new Error("-ToggleModalEventListener()- The source did not have a target");
+            }
+            else {
+                toggleModal(toggle.dataset.target);
+            }
+        }
+        else {
+            throw Error("-ToggleModalEventListener()- The source was not a instance of a HTMLElement.");
+        }
+    }
+    function bindModalToggleEvents() {
+        document.body.querySelectorAll(".modalToggle").forEach((toggle) => {
+            toggle.addEventListener("click", toggleModalEventListener);
+        });
+    }
+    bindModalToggleEvents();
+})();
+//# sourceMappingURL=dialogModal.js.map
